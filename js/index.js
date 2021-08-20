b6fbe6697fbfff6b9a731bd741fdce33e38ed04a
kovshyk <Asdw11wdsaqq>
2021-08-20 19:09:28 +0300

    ;[

---
+++ js/index.js
@@ -0,0 +1,80 @@
+// const message = 'Hello JS';
+// alert(message);
+//
+// let num = 42;
+// alert(num);
+// num+=2;
+// alert(num);
+// const userName = prompt('Enter your name');
+// alert('Hello ' + userName);
+// const promptResult = prompt('Enter your age');
+// const userAge = Number(promptResult);
+// const userAge = Number(prompt('Enter your age'));
+// alert('Your age is ' + userAge);
+// const nextAge = add(userAge,  1);
+// alert('Next year you will be ' + nextAge);
+//
+// Data types
+// Number - Infinity, NaN
+// String - ' ' , " " , ` `
+// boolean - True, False
+//
+// if(userAge>=18){ // > < === !== <= >= || &&
+//     alert('Your are adult')
+// }
+// else{
+//     alert('Your are underage');
+// }
+// function add(a, b){
+//     const result = a+b;
+//     return result;
+// }
+// for (let i=10; i>=1; i--){
+//     console.log(i);
+// }
+// for (let i=0; i<=100; i+=2);
+// {
+//     console.log(i);
+// }
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+const cars = ['nissan juke'];
+cars[2]='BMW x7';
+cars.push('Porsche Taycan');
+cars.shift('tesla x');
+// const sold = cars.pop();
+// console.log(sold);
+//
+// function getRandomCar(cars){
+//     const maxIdx=cars.length - 1;
+//     const chosenIdx = getRandomArbitrary(0, maxIdx);
+//     return cars[chosenIdx];
+// }
+// function getRandomArbitrary(min,max){
+//     return Math.round(Math.random() * (max-min)+min);
+// }
+// console.log(getRandomCar(cars));
+
+// for (let i=0; i<10; i++){
+//     console.log(getRandomCar(cars));
+// }
+
+// for (let i=0; i<cars.length; i++){
+//     console.log(cars[i]);
+// }
+
+// cars.sort();
+// for(const car of cars){
+//     console.log(car);
+// }
+
+// const numbers = [1, 2, 22, 32, 15, 42, 4];
+// numbers.sort((a, b)=> b-a);
+// console.log(numbers);
+
+// function compare (a, b){
+//     return a-b;
+// }
